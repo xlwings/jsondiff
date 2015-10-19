62fc42b5450f4ca7748ab9ee8f3f0fa8a6984706
Eric Reynolds <eric.remo.reynolds@gmail.com>
2015-10-19 12:16:56 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name='jsondiff',
+  packages=['jsondiff'],
+  version='0.1',
+  description='Diff JSON and JSON-like structures in Python',
+  author='Eric Reynolds',
+  author_email='eric.reynolds@zoomeranalytics.com',
+  url='https://github.com/ZoomerAnalytics/jsondiff',
+  download_url='https://github.com/ZoomerAnalytics/jsondiff/tarball/v0.1',
+  keywords=['json', 'diff', 'diffing', 'difference', 'patch', 'delta', 'dict', 'LCS'],
+  classifiers=[],
+)
