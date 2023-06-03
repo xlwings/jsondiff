1a4169f052b5fd5748fd8add39c60c0876591119
Cory Todd <cory.todd@canonical.com>
2023-06-03 10:00:54 -0700

    setup.py: migrate to pyproject.toml
    Embrace the PEP5158 standard but keep on using setuptools. Building is done
    the same way (python -sBm build) so there are no functional changes to
    the build artifacts. The motivation for this change is that switching to
    another build system in future is trivial when using PEP518. An added
    bonus is this file integrates nicely with linters and formatters as
    well.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- setup.py
+++ setup.py
@@ -1,26 +1,4 @@
-import os
-import re
-from setuptools import setup, find_packages
+# Maintained for legacy compatibility
+from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'jsondiff', '__init__.py')) as f:
-    version = re.compile(r".*__version__ = '(.*?)'", re.S).match(f.read()).group(1)
-
-setup(
-    name='jsondiff',
-    packages=find_packages(exclude=['tests']),
-    version=version,
-    description='Diff JSON and JSON-like structures in Python',
-    author='Zoomer Analytics LLC',
-    author_email='eric.reynolds@zoomeranalytics.com',
-    url='https://github.com/ZoomerAnalytics/jsondiff',
-    keywords=['json', 'diff', 'diffing', 'difference', 'patch', 'delta', 'dict', 'LCS'],
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-    ],
-    entry_points={
-        'console_scripts': [
-            'jdiff=jsondiff.cli:main'
-        ]
-    }
-)
+setup()
