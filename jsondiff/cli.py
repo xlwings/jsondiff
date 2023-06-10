59ae21dbf6255fbb7247b1235f53c5007b4adfd4
Cory Todd <cory.todd@canonical.com>
2023-06-10 16:17:58 -0700

    cli: show parameter defaults with --help
    Use ArgumentDefaultsHelpFormatter so we can see what the default
    parameter values are.
    
    Signed-off-by: Cory Todd <cory.todd@canonical.com>

--- jsondiff/cli.py
+++ jsondiff/cli.py
@@ -5,7 +5,7 @@ import warnings
 import sys
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("first")
     parser.add_argument("second")
